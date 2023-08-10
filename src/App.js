a5b78762d0edbe3cc76343016cd01d5ae58d0284
sandeep <sandeep2034maurya@gmail.com>
2023-08-11 00:02:53 +0530

    fixed the overflow of name

--- src/App.js
+++ src/App.js
@@ -2,40 +2,49 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import User from './User';
 import './User_css.css';
-function App() {
-  // const users = ["Sandeep_coding", "invictus109", "tohr.maaal.ke.ch_do"];
-  const [users, setUsers] = useState([]);
-  const renderData = users.map((user) => {
-    return <User username={user} />
-  })
 
+function App() {
+  const [users, setUsers] = useState([]);
   const [name, setName] = useState("");
+
   const addUser = async () => {
     const index = users.indexOf(name);
     if (index !== -1) {
-      alert("Username is already taken!")
-      return
+      alert("Username is already taken!");
+      return;
     }
 
-    let api = `https://codeforces.com/api/user.info?handles=${name}`
+    let api = `https://codeforces.com/api/user.info?handles=${name}`;
     const res = await fetch(api);
-    if (res.ok) { setUsers([...users, name]); }
-    else {
-      alert('Enter valid username!')
+
+    if (res.ok) {
+      const newUserList = [name, ...users];
+      setUsers([name, ...users]);
+      // console.log(newUserList);
+    } else {
+      alert('Enter a valid username!');
     }
-    // setName("");
-    console.log(users);
+    setName("");
   }
+
+  const renderData = users.map((user, index) => (
+    <User key={index} username={user} />
+  ));
   const style = { margin: "0", fontSize: "20px", borderRadius: "2px", cursor: "pointer" };
   return (
     <>
       <form onSubmit={(e) => {
-        e.preventDefault()
+        e.preventDefault();
         addUser();
-      }
-      }>
-        <input type="text" placeholder='Enter username' style={style} value={name} onChange={(e) => setName(e.target.value.toLowerCase())}></input>
-        <button style={style} type="submit">Add User</button>
+      }}>
+        <input
+          type="text"
+          placeholder='Enter username'
+          value={name}
+          style={style}
+          onChange={(e) => setName(e.target.value.toLowerCase())}
+        />
+        <button type="submit" style={style}>Add User</button>
       </form>
       <div id="main">
         {renderData}
--- src/User.js
+++ src/User.js
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import Card from './Card';
+import React, { useEffect, useState } from 'react';
 import './User_css.css';
+
 export default function User(props) {
     let [Data, setData] = useState({});
-    // let api = "https://codeforces.com/api/contest.list?before"
-    let api = `https://codeforces.com/api/user.info?handles=${props.username}`
-    const [name, setName] = useState("city--");
+    console.log(props.username);
+    let api = `https://codeforces.com/api/user.info?handles=${props.username}`;
+
     const fetchApi = async (url) => {
         try {
             const res = await fetch(url);
             const d = await res.json();
             const data = d.result[0];
-            // console.log(data);
             setData(data);
-        }
-        catch (err) {
+        } catch (err) {
             console.log(err);
         }
     }
 
     useEffect(() => {
         fetchApi(api);
-    }, []);
+    }, [api]);
+
     let color = "red";
+    if (Data.rating < 1200) color = "black";
+    else if (Data.rating < 1400) color = "#0fb40c";
+    else if (Data.rating < 1600) color = "cyan";
+    else if (Data.rating < 1800) color = "blue";
+
     const handleClick = () => {
-        console.log("clicked")
         window.open(`https://codeforces.com/profile/${props.username}`, '_blank');
     };
-    if (Data.rating < 1200) color = "black"
-    else if (Data.rating < 1400) color = "green"
-    else if (Data.rating < 1600) color = "cyan"
-    else if (Data.rating < 1800) color = "blue"
+
     return (
         <div className='content' style={{ border: `2px solid ${color}`, cursor: "pointer" }} onClick={handleClick}>
             <div>User Details</div>
             <img src={Data.avatar} style={{ height: "100px" }} alt="profile_pic" />
-            <p style={{ color: color, opacity: "0.6", fontSize: "25px" }}>{Data.rank}</p>
-            <p style={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}>Name: {Data.firstName}  {Data.lastName}</p>
+            <p style={{ color: color, opacity: "0.7", fontSize: "25px" }}>{Data.rank}</p>
+            <p style={{ overflow: "hidden" }}>Name: {Data.firstName}  {Data.lastName}</p>
             <p>Username: {Data.handle}</p>
             <p>Rating: {Data.rating} (Max: {Data.maxRating})</p>
         </div>
-    )
+    );
 }
